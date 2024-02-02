cf1f09a016b3330ccad2c574f3f306b3232d5b55
Sheriff Joe Excel <realexcel2020@gmail.com>
2024-02-02 22:36:38 +0100

    added helm

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo dnf install docker -y
+
+sudo dnf install git -y
+
+sudo systemctl start docker
+
+sudo usermod $USER -aG docker
+
+sudo systemctl enable docker
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
